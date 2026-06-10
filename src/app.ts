9c5cd12668b655eb5e4bdab708e4c561c1f8d964
Leonardo0622 <leonardo.banoy@uniminuto.edu.co>
2026-06-09 23:32:24 -0500

    Add CORS support

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+
 import router from "./routes/auth.routes";
 import ticketRoutes from "./routes/ticket.routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
 
-
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
@@ -15,4 +25,3 @@ app.use("/api/auth", router);
 app.use("/api/tickets", ticketRoutes);
 
 export default app;
-
