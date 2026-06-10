76ea90d4b8b2ee9dd801e019a7d54b5c688608d2
Leonardo0622 <leonardo.banoy@uniminuto.edu.co>
2026-06-09 23:51:42 -0500

    Allow Vercel frontend

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://ticket-system-backend-six.vercel.app"
     ],
     credentials: true
   })
