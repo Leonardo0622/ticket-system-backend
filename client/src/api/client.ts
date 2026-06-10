517ce8236893664ab47d829d2ebedfad308a2178
Leonardo0622 <leonardo.banoy@uniminuto.edu.co>
2026-06-09 23:36:41 -0500

    Add CORS configuration

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("auth_token");
+
   if (token) {
-    config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
-
